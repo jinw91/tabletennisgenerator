843234e4b11c3588f6890e8dad5ec78dc828e413
Nanhua Jin <najin@microsoft.com>
2019-11-18 13:04:51 -0800

    First commit

---
+++ TableTennisGenerator/TableTennisGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TableTennisGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
