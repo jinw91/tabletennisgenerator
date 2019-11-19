68308b21e4d778c5b3a5dbc18f502246527de655
Kate Wilson <kawils@microsoft.com>
2019-11-19 11:29:49 -0800

    style fixes + beginning to add name capability

--- TableTennisGenerator/TableTennisGenerator/Tournament.cs
+++ TableTennisGenerator/TableTennisGenerator/Tournament.cs
@@ -1,43 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace TableTennisGenerator
 {
  
     class Tournament
     {
-        int numPlayers;
-        int numRounds;
-        int simultaneousMatches;
-        Dictionary<int, List<int>> players;
-        Dictionary<int, int> playCounts;
-        Dictionary<int, List<int>> partners;
-        StreamWriter stream;
+        int _numPlayers;
+        int _numRounds;
+        int _simultaneousMatches;
+        Dictionary<int, List<int>> _players;
+        Dictionary<int, int> _playCounts;
+        Dictionary<int, List<int>> _partners;
+        List<string> _playerNames;
+        StreamWriter _stream;
 
         public Tournament(int numPlayers, int numRounds, int simultaneousMatches, string fileDirectory)
         {
             // TODO: error checking of params
-            this.numPlayers = numPlayers;
-            this.numRounds = numRounds;
-            this.simultaneousMatches = simultaneousMatches;
+            _numPlayers = numPlayers;
+            _numRounds = numRounds;
+            _simultaneousMatches = simultaneousMatches;
 
             string fileName = DateTime.Now.ToString("yyyyMMddHHmmss") + ".csv";
-            stream = new StreamWriter(Path.Combine(fileDirectory, fileName), false);
+            _stream = new StreamWriter(Path.Combine(fileDirectory, fileName), false);
 
-            players = InitializeGraph();
+            _playerNames = new List<string>();
+            for (int i=0; i<numPlayers; i++)
+            {
+                _playerNames.Add(i.ToString());
+            }
+
+            _players = InitializeGraph();
             InitializeMetrics();
+        }
+
+        public Tournament(List<string> playerNames, int numRounds, int simultaneousMatches, string fileDirectory) : this(playerNames.Count, numRounds, simultaneousMatches, fileDirectory)
+        {
+            _playerNames = playerNames;
         }
 
         public void InitializeMetrics()
         {
-            playCounts = new Dictionary<int, int>();
-            partners = new Dictionary<int, List<int>>();
+            _playCounts = new Dictionary<int, int>();
+            _partners = new Dictionary<int, List<int>>();
 
-            for (int i = 0; i < numPlayers; i++)
+            for (int i = 0; i < _numPlayers; i++)
             {
-                playCounts.Add(i, 0);
-                partners.Add(i, new List<int>());
+                _playCounts.Add(i, 0);
+                _partners.Add(i, new List<int>());
             }
         }
 
@@ -45,10 +58,10 @@ namespace TableTennisGenerator
         {
             Dictionary<int, List<int>> players = new Dictionary<int, List<int>>();
             // construct fully-connected graph w/ numPlayers nodes
-            for (int i = 0; i < numPlayers; i++)  // TODO: switch to actual player names
+            for (int i = 0; i < _numPlayers; i++)  // TODO: switch to actual player names
             {
                 List<int> otherPlayers = new List<int>();
-                for (int j = 0; j < numPlayers; j++)
+                for (int j = 0; j < _numPlayers; j++)
                 {
                     if (j != i)
                     {
@@ -62,36 +75,36 @@ namespace TableTennisGenerator
 
         public void BuildTournament()
         {
-            stream.WriteLine($"{numPlayers},{numRounds},{simultaneousMatches}");
+            _stream.WriteLine($"{_numPlayers},{_numRounds},{_simultaneousMatches}");
 
-            for (int i = 0; i < numRounds; i++)
+            for (int i = 0; i < _numRounds; i++)
             {
                 BuildRound();
             }
 
-            for (int i=0; i<numPlayers; i++)
+            for (int i=0; i<_numPlayers; i++)
             {
-                Console.WriteLine($"Player {i}: {playCounts[i]} games played");
-                stream.Write($"{i},");
+                Console.WriteLine($"Player {i}: {_playCounts[i]} games played");
+                _stream.Write($"{i},");
                 Console.WriteLine("Partners: ");
-                for (int j = 0; j < partners[i].Count; j++)
+                for (int j = 0; j < _partners[i].Count; j++)
                 {
-                    Console.Write($"{partners[i][j]}, ");
-                    stream.Write($"{partners[i][j]},");
+                    Console.Write($"{_partners[i][j]}, ");
+                    _stream.Write($"{_partners[i][j]},");
                 }
                 Console.WriteLine();
-                stream.WriteLine();
+                _stream.WriteLine();
             }
-            stream.Close();
+            _stream.Close();
         }
 
         public void BuildRound()
         {
-            bool[] visited = new bool[numPlayers];
+            bool[] visited = new bool[_numPlayers];
 
             List<List<Tuple<int, int>>> roundMatches = new List<List<Tuple<int, int>>>();
 
-            for (int i=0; i<simultaneousMatches; i++)
+            for (int i=0; i<_simultaneousMatches; i++)
             {
                 try
                 {
@@ -103,13 +116,13 @@ namespace TableTennisGenerator
                 catch
                 {
                     Console.WriteLine("Resetting graph.");
-                    players = InitializeGraph();
+                    _players = InitializeGraph();
                     foreach (List<Tuple<int, int>> matches in roundMatches)
                     {
                         foreach (Tuple<int, int> pair in matches)
                         {
-                            players[pair.Item1].Remove(pair.Item2);
-                            players[pair.Item2].Remove(pair.Item1);
+                            _players[pair.Item1].Remove(pair.Item2);
+                            _players[pair.Item2].Remove(pair.Item1);
                         }
                     }
                     i--;  // this match didn't actually return players; it just rebuilt the graph so redo it
@@ -120,14 +133,14 @@ namespace TableTennisGenerator
 
         public void CollectMetrics(List<Tuple<int, int>> teams)
         {
-            playCounts[teams[0].Item1]++;
-            playCounts[teams[0].Item2]++;
-            playCounts[teams[1].Item1]++;
-            playCounts[teams[1].Item2]++;
-            partners[teams[0].Item1].Add(teams[0].Item2);
-            partners[teams[0].Item2].Add(teams[0].Item1);
-            partners[teams[1].Item1].Add(teams[1].Item2);
-            partners[teams[1].Item2].Add(teams[1].Item1);
+            _playCounts[teams[0].Item1]++;
+            _playCounts[teams[0].Item2]++;
+            _playCounts[teams[1].Item1]++;
+            _playCounts[teams[1].Item2]++;
+            _partners[teams[0].Item1].Add(teams[0].Item2);
+            _partners[teams[0].Item2].Add(teams[0].Item1);
+            _partners[teams[1].Item1].Add(teams[1].Item2);
+            _partners[teams[1].Item2].Add(teams[1].Item1);
         }
 
         public List<Tuple<int, int>> BuildMatch(bool[] visited)
@@ -144,10 +157,10 @@ namespace TableTennisGenerator
                 visited[team1.Item2] = false;
                 throw;
             }
-            players[team1.Item1].Remove(team1.Item2);
-            players[team1.Item2].Remove(team1.Item1);
-            players[team2.Item1].Remove(team2.Item2);
-            players[team2.Item2].Remove(team2.Item1);
+            _players[team1.Item1].Remove(team1.Item2);
+            _players[team1.Item2].Remove(team1.Item1);
+            _players[team2.Item1].Remove(team2.Item2);
+            _players[team2.Item2].Remove(team2.Item1);
 
             return new List<Tuple<int, int>> { team1, team2 };
         }
@@ -167,32 +180,40 @@ namespace TableTennisGenerator
 
         public int FindMostConnectedPlayer(bool[] visited)
         {
-            int max = 0;
-            int maxPlayer = -1;
-            foreach (KeyValuePair<int, List<int>> player in players)
-            {
-                if (player.Value.Count > max && !visited[player.Key])
-                {
-                    max = player.Value.Count;
-                    maxPlayer = player.Key;
-                }
-            }
-            return maxPlayer;
+            return FindRandomMaxPlayer(_players.Keys.ToList(), visited);
         }
 
         public int FindPartner(int player, bool[] visited)
+        { 
+            return FindRandomMaxPlayer(_players[player], visited);
+        }
+
+        public int FindRandomMaxPlayer(List<int> possiblePlayers, bool[] visited)
         {
-            int partner = -1;
             int max = 0;
-            foreach (int p in players[player])
+            List<int> maxPlayers = new List<int>();
+            foreach (int p in possiblePlayers)
             {
-                if (!visited[p] && players[p].Count > max)
+                if (!visited[p])
                 {
-                    partner = p;
-                    max = players[p].Count;
+                    if (_players[p].Count == max)
+                    {
+                        maxPlayers.Add(p);
+                    }
+                    else if (_players[p].Count > max)
+                    {
+                        maxPlayers = new List<int> { p };
+                        max = _players[p].Count;
+                    }
                 }
             }
-            return partner;
+            int mostConnectedPlayer = -1;
+            if (maxPlayers.Count > 0)
+            {
+                Random rand = new Random();
+                mostConnectedPlayer = maxPlayers[rand.Next(maxPlayers.Count)];
+            }
+            return mostConnectedPlayer;
         }
         
     }
