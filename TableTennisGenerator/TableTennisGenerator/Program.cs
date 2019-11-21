a0b60f643556c3f89231ed165589a1b5bcba39e0
Kate Wilson <kawils@microsoft.com>
2019-11-21 09:53:39 -0800

    setup for multiple runs for testing

--- TableTennisGenerator/TableTennisGenerator/Program.cs
+++ TableTennisGenerator/TableTennisGenerator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace TableTennisGenerator
 {
@@ -7,12 +8,15 @@ namespace TableTennisGenerator
     {
         static void Main(string[] args)
         {
+            int numTournaments = 100;
+            int numRounds = 30;
+            string output_dir = "C:\\output\\tournament\\" + $"{numTournaments}_tournaments_{numRounds}_rounds";
+            Directory.CreateDirectory(output_dir);
             Random rand = new Random();
-            for (int i=0; i<10; i++)
+            for (int i=0; i<numTournaments; i++)
             {
                 int numPlayers = rand.Next(8, 100);
-                int numRounds = 30;
-                Tournament tournament = new Tournament(numPlayers, numRounds, 2, "C:\\output\\tournament", true);
+                Tournament tournament = new Tournament(numPlayers, numRounds, 2, output_dir, true);
                 tournament.BuildTournament();
             }
         }
